011247eb8e1e8bf0b091a674a7d0e5ac85a49d68
Mathias Rahbek-Borre (MTRA) <mtra@novozymes.com>
2024-02-05 19:11:07 +0100

    first commit

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,16 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+--
+vim.opt.clipboard = "unnamedplus"
+
+if vim.fn.has("wsl") == 1 then
+  vim.api.nvim_create_autocmd("TextYankPost", {
+
+    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+
+    callback = function()
+      vim.fn.system("clip.exe", vim.fn.getreg('"'))
+    end,
+  })
+end
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,46 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", function()
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+
+vim.keymap.set({ "i", "v" }, "jk", "<ESC>")
+
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function()
+  harpoon:list():append()
+end)
+vim.keymap.set("n", "<C-e>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+vim.keymap.set("n", "<C-h>", function()
+  harpoon:list():select(1)
+end)
+vim.keymap.set("n", "<C-t>", function()
+  harpoon:list():select(2)
+end)
+vim.keymap.set("n", "<C-n>", function()
+  harpoon:list():select(3)
+end)
+vim.keymap.set("n", "<C-s>", function()
+  harpoon:list():select(4)
+end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function()
+  harpoon:list():prev()
+end)
+vim.keymap.set("n", "<C-S-N>", function()
+  harpoon:list():next()
+end)
